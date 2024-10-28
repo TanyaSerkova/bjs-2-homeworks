ba09a88939f2601f04aa37cfc6b94a782d46d4cc
Tanya Serkova <tanyakim15@rambler.ru>
2024-10-28 16:28:24 +0300

    task.js правки после проверки

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,24 +1,20 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  let D = b**2 - 4 * a * c;
+  let d = b**2 - 4 * a * c;
 
-  if (D === 0) {
+  if (d === 0) {
     arr.push(-b / (2 * a));
-    return arr;
-  } else if (D > 0) {
-    arr.push((-b + Math.sqrt(D) ) / (2 * a))
-    &&
-    arr.push((-b - Math.sqrt(D) )/(2*a))
-    return arr
+  } else if (d > 0) {
+    arr.push((-b + Math.sqrt(d) ) / (2 * a));
+    arr.push((-b - Math.sqrt(d) ) / (2 * a))
   }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  percent = percent / 100 / 12;
+  let percentMonth = percent / 100 / 12;
   let creditBody = amount - contribution;
-  let payment = creditBody * (percent + (percent / (((1 + percent)**countMonths) - 1)));
-  let totalMortgage = (contribution + creditBody + (creditBody - payment * countMonths)).toFixed(2);
-  return Number(totalMortgage);
+  let payment = creditBody * (percentMonth + (percentMonth / (((1 + percentMonth)**countMonths) - 1)));
+  return Number((payment * countMonths).toFixed(2));
 }
